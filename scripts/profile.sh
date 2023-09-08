31c5a0496ec05a1ed843166af8b9417bfb241fa0
seyoung5744 <seyoung574458@gmail.com>
2023-09-08 20:13:06 +0900

    test

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -30,7 +30,7 @@ function find_idle_profile() {
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
-
+    echo -e "> 쉬고 있는 profile : $IDLE_PROFILE"
     if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
